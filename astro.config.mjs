2a0bcd391fcbb90e2728e2b5eeacd2fb2056178d
peterpoy <ppoydenko@gmail.com>
2026-05-04 15:40:30 -0400

    tailwind upgrade checkpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import alpinejs from "@astrojs/alpinejs";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
@@ -8,14 +8,16 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
     integrations: [
-      tailwind(),
-      alpinejs(),
-      mdx(),
-      (await import("@playform/inline")).default({
-          Critters: true,
-      }),
-      sitemap({lastmod: new Date('2022-02-24')}),
+        alpinejs(),
+        mdx(),
+        (await import("@playform/inline")).default(/** @type {any} */({
+            Critters: true,
+        })),
+        sitemap({ lastmod: new Date('2022-02-24') }),
     ],
+    vite: {
+        plugins: [tailwindcss()],
+    },
     output: "static",
     devToolbar: {
         enabled: false,
